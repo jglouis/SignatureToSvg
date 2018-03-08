b3356cbb48b931c6d97fcc2eac8fd8dd82a4a7b0
Jean-Guillaume Louis <jguillaume.louis@gmail.com>
2018-03-08 12:30:05 +0100

    Add layout with "Save" button

---
+++ app/src/main/java/xyz/hexode/signaturetosvg/DrawingActivity.kt
@@ -0,0 +1,126 @@
+package xyz.hexode.signaturetosvg
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Path
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.MotionEvent
+import android.view.View
+import kotlinx.android.synthetic.main.activity_drawing.*
+
+const val TOUCH_TOLERANCE = 4f
+
+/**
+ * Created by JGLouis on 08/03/2018.
+ *
+ * Drawing activity
+ */
+class DrawingActivity : AppCompatActivity() {
+
+    private var mPaint: Paint? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val dv = DrawingView(this)
+        setContentView(R.layout.activity_drawing)
+        drawingLayout.addView(dv)
+        mPaint = Paint()
+        mPaint!!.isAntiAlias = true
+        mPaint!!.isDither = true
+        mPaint!!.color = Color.GREEN
+        mPaint!!.style = Paint.Style.STROKE
+        mPaint!!.strokeJoin = Paint.Join.ROUND
+        mPaint!!.strokeCap = Paint.Cap.ROUND
+        mPaint!!.strokeWidth = 12f
+    }
+
+    inner class DrawingView(context: Context) : View(context) {
+
+        private var mBitmap: Bitmap? = null
+        private var mCanvas: Canvas? = null
+        private val mPath: Path = Path()
+        private val mBitmapPaint: Paint = Paint(Paint.DITHER_FLAG)
+        private val circlePaint: Paint = Paint()
+        private val circlePath: Path = Path()
+
+        private var mX: Float = 0f
+        private var mY: Float = 0f
+
+        init {
+            circlePaint.isAntiAlias = true
+            circlePaint.color = Color.BLUE
+            circlePaint.style = Paint.Style.STROKE
+            circlePaint.strokeJoin = Paint.Join.MITER
+            circlePaint.strokeWidth = 4f
+        }
+
+        override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+            super.onSizeChanged(w, h, oldw, oldh)
+
+            mBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
+            mCanvas = Canvas(mBitmap!!)
+        }
+
+        override fun onDraw(canvas: Canvas) {
+            super.onDraw(canvas)
+
+            canvas.drawBitmap(mBitmap!!, 0f, 0f, mBitmapPaint)
+            canvas.drawPath(mPath, mPaint!!)
+            canvas.drawPath(circlePath, circlePaint)
+        }
+
+        private fun touchStart(x: Float, y: Float) {
+            mPath.reset()
+            mPath.moveTo(x, y)
+            mX = x
+            mY = y
+        }
+
+        private fun touchMove(x: Float, y: Float) {
+            val dx = Math.abs(x - mX)
+            val dy = Math.abs(y - mY)
+            if (dx >= TOUCH_TOLERANCE || dy >= TOUCH_TOLERANCE) {
+                mPath.quadTo(mX, mY, (x + mX) / 2, (y + mY) / 2)
+                mX = x
+                mY = y
+
+                circlePath.reset()
+                circlePath.addCircle(mX, mY, 30f, Path.Direction.CW)
+            }
+        }
+
+        private fun touchUp() {
+            mPath.lineTo(mX, mY)
+            circlePath.reset()
+            // commit the path to our offscreen
+            mCanvas!!.drawPath(mPath, mPaint!!)
+            // kill this so we don't double draw
+            mPath.reset()
+        }
+
+        override fun onTouchEvent(event: MotionEvent): Boolean {
+            val x = event.x
+            val y = event.y
+
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    touchStart(x, y)
+                    invalidate()
+                }
+                MotionEvent.ACTION_MOVE -> {
+                    touchMove(x, y)
+                    invalidate()
+                }
+                MotionEvent.ACTION_UP -> {
+                    touchUp()
+                    invalidate()
+                }
+            }
+            return true
+        }
+    }
+}
